c93982c7617a994c0006c953c1d21913c8eecbc5
kevinqqk-tech <kevinq.qk@gmail.com>
2026-05-27 20:48:21 +0800

    feat: deploy

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
 /// <reference types="vitest/config" />
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   esbuild: {
-    jsx: 'automatic',
+    jsx: "automatic",
   },
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.js',
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.js",
   },
-})
+  base: mode === "production" ? "/se-take-home-assignment/" : "/",
+}));
